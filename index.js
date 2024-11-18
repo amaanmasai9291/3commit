eff1efd07d0a0b847f1c3d501e9f3869fa4111e0
Amaan Khan <amaankhan9291@gmail.com>
2024-11-18 12:46:38 +0530

    Add code to print a pattern

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ for (let i = 0; i < numbers.length; i++) {
     }
 }
 console.log('Odd numbers:', odds);
+
+const n = 5;
+for (let i = 1; i <= n; i++) {
+    let stars = '';
+    for (let j = 1; j <= i; j++) {
+        stars += '*';
+    }
+    console.log(stars);
+}
