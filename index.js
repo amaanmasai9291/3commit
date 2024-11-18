cdb1608099bd08b844e01e58274a1febe2fc0777
Amaan Khan <amaankhan9291@gmail.com>
2024-11-18 12:45:19 +0530

    Add code to find even numbers in an array

---
+++ index.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const evens = [];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evens.push(numbers[i]);
+    }
+}
+console.log('Even numbers:', evens);
+
